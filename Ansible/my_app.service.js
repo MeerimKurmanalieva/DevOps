0720622e454e641c96d42b57117af8d9926694c6
Meri <mamakeeva.m93@gmail.com>
2023-06-09 10:56:37 -0400

    homework

---
+++ Ansible/my_app.service.js
@@ -0,0 +1,14 @@
+[Unit]
+Description=Your Application Service
+After=network.target
+
+    [Service]
+ExecStart=ExecStart=/home/mkurmanalieva/myapp/main
+WorkingDirectory=/home/mkurmanalieva/myapp
+
+User=meerimkurmanalieva
+Restart=always
+
+    [Install]
+WantedBy=multi-user.target
+
