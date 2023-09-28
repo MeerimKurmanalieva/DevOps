6d098cba0eef8c8f8e2a618b401335979a030b56
Meri <mamakeeva.m93@gmail.com>
2023-09-27 22:21:38 -0400

    backend

--- hello-back-app/main.go
+++ hello-back-app/main.go
@@ -4,7 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 )
+
+var instanceID string
 
 type HelloRequest struct {
 	Name string `json:"name"`
@@ -15,7 +24,11 @@ type HelloResponse struct {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the JSON request body into a HelloRequest struct
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
 	var request HelloRequest
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&request); err != nil {
@@ -23,15 +36,11 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Create a response message
 	message := fmt.Sprintf("Hello, %s!", request.Name)
-
-	// Create a JSON response
 	response := HelloResponse{
 		Message: message,
 	}
 
-	// Encode and send the response
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(response); err != nil {
 		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
@@ -39,8 +48,110 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func createEC2Handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	awsAccessKey := os.Getenv("AWS_ACCESS_KEY_ID")
+	awsSecretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	fmt.Println("AWS Access Key:", awsAccessKey)
+	fmt.Println("AWS Secret Key:", awsSecretKey)
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-1"), // Change to your desired region
+		Credentials: credentials.NewStaticCredentials(awsAccessKey, awsSecretKey, ""),
+	})
+	if err != nil {
+		http.Error(w, "Failed to create AWS session", http.StatusInternalServerError)
+		return
+	}
+
+	ec2Svc := ec2.New(sess)
+
+	runInput := &ec2.RunInstancesInput{
+		ImageId:          aws.String("ami-03a6eaae9938c858c"),
+		InstanceType:     aws.String("t2.micro"),
+		MinCount:         aws.Int64(1),
+		MaxCount:         aws.Int64(1),
+		KeyName:          aws.String("myKey"),
+		SecurityGroupIds: []*string{aws.String("sg-0b71386f66ac0b359")}, // Add your security group ID
+		SubnetId:         aws.String("subnet-091a5ba500af5a208"),        // Add your VPC ID
+	}
+
+	// Create the EC2 instance
+	result, err := ec2Svc.RunInstances(runInput)
+	if err != nil {
+		fmt.Println("Error creating EC2 instance:", err)
+		http.Error(w, "Error creating EC2 instance", http.StatusInternalServerError)
+		return
+	}
+
+	// Store the instance ID
+	instanceID = *result.Instances[0].InstanceId
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "EC2 instance creation request received.")
+}
+
+func terminateEC2Handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if instanceID == "" {
+		http.Error(w, "No EC2 instance to terminate", http.StatusBadRequest)
+		return
+	}
+
+	awsAccessKey := os.Getenv("AWS_ACCESS_KEY_ID")
+	awsSecretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	fmt.Println("AWS Access Key:", awsAccessKey)
+	fmt.Println("AWS Secret Key:", awsSecretKey)
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-1"), // Change to your desired region
+		Credentials: credentials.NewStaticCredentials(awsAccessKey, awsSecretKey, ""),
+	})
+	if err != nil {
+		http.Error(w, "Failed to create AWS session", http.StatusInternalServerError)
+		return
+	}
+
+	ec2Svc := ec2.New(sess)
+
+	terminateInput := &ec2.TerminateInstancesInput{
+		InstanceIds: []*string{aws.String(instanceID)},
+	}
+
+	_, err = ec2Svc.TerminateInstances(terminateInput)
+	if err != nil {
+		fmt.Println("Error terminating EC2 instance:", err)
+		http.Error(w, "Error terminating EC2 instance", http.StatusInternalServerError)
+		return
+	}
+
+	// Clear the stored instance ID
+	instanceID = ""
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "EC2 instance termination request received.")
+}
+
 func main() {
 	http.HandleFunc("/api/hello", helloHandler)
-	fmt.Println("Server is listening on :8080")
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/api/ec2/create", createEC2Handler)
+	http.HandleFunc("/api/ec2/terminate", terminateEC2Handler) // Add termination endpoint
+
+	port := ":8080"
+	if len(os.Args) >= 2 && strings.HasPrefix(os.Args[1], ":") {
+		port = os.Args[1]
+	}
+
+	fmt.Printf("Server is listening on %s\n", port)
+	http.ListenAndServe(port, nil)
 }
